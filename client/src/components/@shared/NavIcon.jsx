79dc324919c3b69776c542e032788154f1b8333a
manitramanitra <randiluc@gmail.com>
2024-01-30 15:15:17 +0300

    ajoute du packet nodemaileré

--- client/src/components/@shared/NavIcon.jsx
+++ client/src/components/@shared/NavIcon.jsx
@@ -4,17 +4,20 @@ import { NavLink } from 'react-router-dom'
 function NavIcon() {
   return (
     <div className="let-nav-container">
-        <div className="icons">
+        <div className="icons bg-red">
             <div className="icons-bis">
-                <NavLink to="/" className="active-left-nav">
+                <NavLink to="/" className={({isActive})=>
+                 `active-left-nav ${isActive ? "bg-#fdeeee":""}`}>
                     <img src="./img/icons/home.svg" alt="" />
                 </NavLink>
                 <br />
-                <NavLink to="/trending" >
+                <NavLink to="/trending" className={({isActive})=>
+                 `active-left-nav ${isActive ? "bg-#fdeeee":""}`}>
                     <img src="./img/icons/rocket.svg" alt="" />
                 </NavLink>
                 <br />
-                <NavLink to="/profil" >
+                <NavLink to="/profil" className={({isActive})=>
+                 `${isActive ? "bg-red":""}`}>
                     <img src="./img/icons/user.svg" alt="" />
                 </NavLink>
             </div>
